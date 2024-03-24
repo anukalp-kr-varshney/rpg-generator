85a6ee71027bf95b3609673702d456073fafc82f
anukalp-kr-varshney <158707645+anukalp-kr-varshney@users.noreply.github.com>
2024-03-24 14:21:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const passworBox = document.getElementById('password');
+const length = 14;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "12345678910";
+const symbol = "!@#$%^&*(){}[]:;<>?";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passworBox.value = password;
+
+}
+
+
+function copyPassword(){
+    passworBox.select();
+    document.execCommand("copy");
+}
